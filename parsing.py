75f5b304dde2ac41d094fee772d8f3fb2d7cc705
Aerobreaker <Aerobreaker@gmail.com>
2019-09-20 13:04:35 -0700

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,5 +1,5 @@
 #one-line:
-#print('{}\n'.format(''.join('\nDuplicate found!\n{}'.format(id) for (id, cnt) in __import__('collections').Counter(tuple(__import__('itertools').compress(line.strip().split(','), cols)) for cols, line in zip(__import__('itertools').repeat(tuple(__import__('itertools').chain.from_iterable(__import__('itertools').chain([i[0] for i in zip(__import__('itertools').repeat(0), __import__('itertools').takewhile(lambda x: x < j-1, c))], [1]) for j, c in eval('zip(sorted(int(k) for k in {0}.split(",")), [__import__("itertools").count()]*len({0}.split(",")))'.format(repr(input('Please input column numbers delimited by comma <1,2>:\n') or '1,2')))))), open(input('Input file path:\n')))).most_common(input('Show all duplicates (Yes/<N>o)?\n')[:1].lower()!='y' or None) if cnt > 1) or '\nNo duplicates found.'))
+#print('{}\n'.format(''.join('\nDuplicate found!\n{}'.format(id) for (id, cnt) in __import__('collections').Counter(tuple(__import__('itertools').compress(line.strip().split(','), cols)) for cols, line in zip(__import__('itertools').repeat([[1 if i+1 in set(s) else 0 for i in range(max(s))] for s in [[int(i) for i in (input('Please input column numbers delimited by comma <1,2>:\n') or '1,2').split(',')]]][0]), open(input('Input file path:\n')))).most_common(input('Show all duplicates (Yes/<N>o)?\n')[:1].lower()!='y' or None) if cnt > 1) or '\nNo duplicates found.'))
 
 from itertools import compress
 seen = set()
@@ -22,3 +22,5 @@ with open(input('Input file path:\n')) as file:
     if not shown:
         print('No duplicates found.')
     print()
+
+
