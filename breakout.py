75a59da11530cf4959dc10bedd5f5b95517eb67f
Aerobreaker <Aerobreaker@gmail.com>
2019-09-24 13:26:44 -0700

    Update parsing.py

--- breakout.py
+++ breakout.py
@@ -20,3 +20,9 @@ NameError: name '__import__' is not defined
 
 Python>echo/This is a batch file that does bad stuff
 This is a batch file that does bad stuff
+
+
+
+
+
+exec("[print('\\n{}\\n'.format('\\n'.join(*(['Duplicate found!\\n{}'.format(id) for (id, cnt) in __import__('collections').Counter(tuple(__import__('itertools').compress(line.strip().split(','), cols)) for cols, line in zip(__import__('itertools').repeat([[1 if i+1 in set(s) else 0 for i in range(max(s))] for s in [[int(i) for i in (input('Please input column numbers delimited by comma <1,2>:\\n') or '1,2').split(',')]]][0]), open(input('Input file path:\\n')))).most_common(num) if cnt > 1] for num in [input('\nShow all duplicates (Yes/<N>o)?\\n')[:1].lower()!='y' or None])) or 'No duplicates found.')) for __import__, print, input, zip, int, range, max, set, open, tuple in [[[i for i in ().__class__.__bases__[0].__subclasses__() if i.__name__ == 'BuiltinImporter'][0].load_module('builtins').__dict__[i] for i in ('__import__', 'print', 'input', 'zip', 'int', 'range', 'max', 'set', 'open', 'tuple')]]]", {'__builtins__':{}})
--- parsing.py
+++ parsing.py
@@ -1,10 +1,10 @@
 #one-line:
-#print('\n{}\n'.format('\n'.join(*(['Duplicate found!\n{}'.format(id) for (id, cnt) in __import__('collections').Counter(tuple(__import__('itertools').compress(line.strip().split(','), cols)) for cols, line in zip(__import__('itertools').repeat([[1 if i+1 in set(s) else 0 for i in range(max(s))] for s in [[int(i) for i in (input('Please input column numbers delimited by comma <1,2>:\n') or '1,2').split(',')]]][0]), open(input('Input file path:\n')))).most_common(num) if cnt > 1] for num in [input('Show all duplicates (Yes/<N>o)?\n')[:1].lower()!='y' or None])) or 'No duplicates found.'))
+#print('\n{}\n'.format('\n'.join(*(['Duplicate found!\n{}'.format(id) for (id, cnt) in __import__('collections').Counter(tuple(__import__('itertools').compress(line.strip().split(','), cols)) for cols, line in zip(__import__('itertools').repeat([[1 if i+1 in set(s) else 0 for i in range(max(s))] for s in [[int(i) for i in (input('Please input column numbers delimited by comma <1,2>:\n') or '1,2').split(',')]]][0]), open(input('Input file path:\n')))).most_common(num) if cnt > 1] for num in [input('\nShow all duplicates (Yes/<N>o)?\n')[:1].lower()!='y' or None])) or 'No duplicates found.'))
 
 from itertools import compress
 seen = set()
 shown = set()
-showall = input('Show all duplicates (Yes/<N>o)?\n')[:1].lower()
+showall = input('\nShow all duplicates (Yes/<N>o)?\n')[:1].lower()
 columns = input('Input column numbers, delimited by comma <1,2>:\n') or '1,2'
 columns = set(int(i) for i in columns.split(','))
 columns = [1 if i + 1 in columns else 0 for i in range(max(columns))]
